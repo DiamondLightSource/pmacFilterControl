aff4d09b89ad1d3c0f1b330ca57aaee7756b6353
Gary Yendell <gary.yendell@diamond.ac.uk>
2022-02-28 17:09:32 +0000

    Use add_compile_options instead of CMAKE_CXX_FLAGS directly
    - This avoids duplicates and only inserts it when compiling and not linking

--- cmake/arm-gcc-toolchain.cmake
+++ cmake/arm-gcc-toolchain.cmake
@@ -10,7 +10,7 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
-# Set flags for PowerPMAC ARM architecture
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=armv7-a -mtune=cortex-a9 -mfpu=vfpv3-d16 -mfloat-abi=hard")
-# Prevent warnings from gcc version
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fcompare-debug-second")
+# Set flags for PowerPMAC ARM
+add_compile_options(-march=armv7-a -mtune=cortex-a9 -mfpu=vfpv3-d16 -mfloat-abi=hard)
+# Hide 'changed in GCC 7.1' warnings from nlohmann::json
+add_compile_options(-fcompare-debug-second)
