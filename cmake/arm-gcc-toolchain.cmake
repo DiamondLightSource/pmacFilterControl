e2a7c06b8f068016162d45df58d259985d783c4a
Gary Yendell <gary.yendell@diamond.ac.uk>
2022-02-28 16:21:01 +0000

    Update ARM toolchain to better match ppmac
    - Based on Emilio digging around in the ppmac IDE toolchain

--- cmake/arm-gcc-toolchain.cmake
+++ cmake/arm-gcc-toolchain.cmake
@@ -1,8 +1,8 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR ARM)
 
-SET(CMAKE_C_COMPILER arm-linux-gnueabi-gcc)
-SET(CMAKE_CXX_COMPILER arm-linux-gnueabi-g++)
+SET(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
+SET(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
 
 # Search options
 set(CMAKE_FIND_ROOT_PATH /build)
@@ -10,5 +10,7 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
+# Set flags for PowerPMAC ARM architecture
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=armv7-a -mtune=cortex-a9 -mfpu=vfpv3-d16 -mfloat-abi=hard")
 # Prevent warnings from gcc version
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fcompare-debug-second")
