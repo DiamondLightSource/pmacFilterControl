89c2303a6eec81f34c505b479fb0983d01eb744a
Gary Yendell <gary.yendell@diamond.ac.uk>
2022-02-28 17:14:53 +0000

    Tidying

--- tools/run_arm_container.sh
+++ tools/run_arm_container.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-CONTAINER_RUN="podman run --rm -it -v ${PMACFILTERCONTROL_ROOT}/:/build/pmacFilterControl --privileged --net=host arm-crosscompiler"
+ARM_PMACFILTERCONTROL="/build/pmacFilterControl"
+ARM_BUILD="${ARM_PMACFILTERCONTROL}/arm_build"
+ARM_PREFIX="${ARM_PMACFILTERCONTROL}/arm_prefix"
+
+CONTAINER_RUN="podman run --rm -it -v ${PMACFILTERCONTROL}:${ARM_PMACFILTERCONTROL} --privileged --net=host arm-crosscompiler"
 
 if [ -z "$1" ]; then
   ${CONTAINER_RUN}
 elif [ $1 == "bash" ]; then
   ${CONTAINER_RUN} bash
 elif [ $1 == "build" ]; then
-  ${CONTAINER_RUN} bash -c "mkdir -p /build/pmacFilterControl/arm_build && cd /build/pmacFilterControl/arm_build && mkdir -p /build/pmacFilterControl/arm_prefix && cmake .. -DCMAKE_TOOLCHAIN_FILE=/build/pmacFilterControl/cmake/arm-gcc-toolchain.cmake -DCMAKE_INSTALL_PREFIX=/build/pmacFilterControl/arm_prefix -DZEROMQ_ROOTDIR=/build/libzmq/prefix/ && make VERBOSE=1 && make install"
+  ${CONTAINER_RUN} bash -c "\
+mkdir -p ${ARM_BUILD} && rm -rf ${ARM_BUILD}/* && cd ${ARM_BUILD} && \
+mkdir -p ${ARM_PREFIX} && rm -rf ${ARM_PREFIX}/* && \
+cmake .. -DCMAKE_TOOLCHAIN_FILE=${ARM_PMACFILTERCONTROL}/cmake/arm-gcc-toolchain.cmake -DCMAKE_INSTALL_PREFIX=${ARM_PREFIX} -DZEROMQ_ROOTDIR=/build/libzmq/prefix/ && \
+make VERBOSE=1 && make install"
 elif [ $1 == "pmacFilterControl" ]; then
-  ${CONTAINER_RUN} qemu-arm -L /usr/arm-linux-gnueabihf/ /build/pmacFilterControl/arm_prefix/bin/pmacFilterControl 10001 127.0.0.1:10000
+  ${CONTAINER_RUN} qemu-arm -L /usr/arm-linux-gnueabihf/ ${ARM_PREFIX}/bin/pmacFilterControl 10001 127.0.0.1:10000
 else
   echo "Unknown command: '$1'"
   exit 1
