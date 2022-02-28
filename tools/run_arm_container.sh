0d0b6182eddd08d6b81db8bf0dbbed8f3cb73e21
Gary Yendell <gary.yendell@diamond.ac.uk>
2022-02-28 16:37:38 +0000

    Rename binary in CMakeLists

--- tools/run_arm_container.sh
+++ tools/run_arm_container.sh
@@ -9,7 +9,7 @@ elif [ $1 == "bash" ]; then
 elif [ $1 == "build" ]; then
   ${CONTAINER_RUN} bash -c "mkdir -p /build/pmacFilterControl/arm_build && cd /build/pmacFilterControl/arm_build && mkdir -p /build/pmacFilterControl/arm_prefix && cmake .. -DCMAKE_TOOLCHAIN_FILE=/build/pmacFilterControl/cmake/arm-gcc-toolchain.cmake -DCMAKE_INSTALL_PREFIX=/build/pmacFilterControl/arm_prefix -DZEROMQ_ROOTDIR=/build/libzmq/prefix/ && make VERBOSE=1 && make install"
 elif [ $1 == "pmacFilterControl" ]; then
-  ${CONTAINER_RUN} qemu-arm -L /usr/arm-linux-gnueabihf/ /build/pmacFilterControl/arm_prefix/bin/pmacFilterController 10001 127.0.0.1:10000
+  ${CONTAINER_RUN} qemu-arm -L /usr/arm-linux-gnueabihf/ /build/pmacFilterControl/arm_prefix/bin/pmacFilterControl 10001 127.0.0.1:10000
 else
   echo "Unknown command: '$1'"
   exit 1
