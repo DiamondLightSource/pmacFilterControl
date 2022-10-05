e227cee6c403324c4bc6c73aae244c2db7fe095b
Gary Yendell <gary.yendell@diamond.ac.uk>
2022-10-05 16:37:00 +0100

    Reset VERSION number to 0.1 for first deployed version

--- src/pmacFilterControl.cpp
+++ src/pmacFilterControl.cpp
@@ -11,7 +11,7 @@
 #include "gplib.h"
 #endif
 
-#define VERSION "0.9"
+#define VERSION "0.1"
 
 const int MAX_ATTENUATION = 15;  // All filters in: 1 + 2 + 4 + 8
 const long POLL_TIMEOUT = 100;  // Length of ZMQ poll in milliseconds
