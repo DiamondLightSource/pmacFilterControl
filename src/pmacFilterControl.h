bcb211adf6dc5b10cc1f04e5eb13584df57a4edc
Gary Yendell <gary.yendell@diamond.ac.uk>
2022-10-24 10:52:36 +0100

    Fix doc strings for parameters so doxygen finds them

--- src/pmacFilterControl.h
+++ src/pmacFilterControl.h
@@ -40,62 +40,66 @@ class PMACFilterController
 
     private:
         /* ZMQ */
-        // Endpoint for control channel
+        /** Endpoint for control channel */
         std::string control_channel_endpoint_;
-        // Endpoint for event stream publish channel
+        /** Endpoint for event stream publish channel */
         std::string publish_channel_endpoint_;
-        // Endpoints for data message subscribe channels
+        /** Endpoints for data message subscribe channels */
         std::vector<std::string> subscribe_channel_endpoints_;
-        // Context
+        /** ZMQ Context */
         zmq::context_t zmq_context_;
-        // Sockets
+        /** ZMQ Socket for control chnanel */
         zmq::socket_t zmq_control_socket_;
+        /** ZMQ Socket for publish chnanel */
         zmq::socket_t zmq_publish_socket_;
+        /** ZMQ Socket for subscribe chnanels */
         std::vector<zmq::socket_t> zmq_subscribe_sockets_;
 
         /* Internal Logic */
-        // The current logic state
+        /** The current logic state */
         ControlState state_;
-        // The frame number of the last message that was received, but not necessarily processed - used to determine
-        // that the attenuation level is stable in single-shot mode
+        /** The frame number of the last message that was received, but not necessarily processed - used to determine
+            that the attenuation level is stable in single-shot mode */
         int64_t last_received_frame_;
-        // The frame number of the last message that was successfully processed - used to decide to ignore some frames
+        /** The frame number of the last message that was successfully processed - used to decide to ignore some
+         *  frames */
         int64_t last_processed_frame_;
-        // Time of last message received - not necessarily causing processing
+        /** Time of last message received - not necessarily causing processing */
         struct timespec last_message_ts_;
-        // Time of last process of a message
+        /** Time of last process of a message */
         struct timespec last_process_ts_;
-        // Duration in microseconds of previous process
+        /** Duration in microseconds of previous process */
         size_t process_duration_;
-        // Time elapsed in microseconds from one process to the next. This will include any time spent waiting for
-        // messages and other housekeeping
+        /** Time elapsed in microseconds from one process to the next. This will include any time spent waiting for
+         * messages and other housekeeping */
         size_t process_period_;
-        // Flag to start a new single shot run
+        /** Flag to start a new single shot run */
         bool singleshot_start_;
-        // Flag to reset timeout and re-enter continuous mode
+        /** Flag to reset timeout and re-enter continuous mode */
         bool clear_timeout_;
-        // Flag to interrupt listen loop and shutdown process
+        /** Flag to interrupt listen loop and shutdown process */
         bool shutdown_;
-        // Thread to subscribe to data channel and control filters
+        /** Thread to subscribe to data channel and control filters */
         std::thread subscribe_thread_;
 
         /* Filter Logic */
-        // Local store of current attenuation to compare against the next attenuation change request
+        /** Local store of current attenuation to compare against the next attenuation change request */
         int current_attenuation_;
-        // Filter positions from previous process for calculation of positions after filter in move
+        /** Filter positions from previous process for calculation of positions after filter in move */
         std::vector<int> current_demand_;
-        // Filter positions after filter in move applied
+        /** Filter positions after filter in move applied */
         std::vector<int> post_in_demand_;
-        // Filter positions after filter in and out moves applied
+        /** Filter positions after filter in and out moves applied */
         std::vector<int> final_demand_;
 
         /* Control Channel Parameters */
-        // The current mode of operation
+        /** The current mode of operation */
         ControlMode mode_;
-        // Filter in and out positions in counts (can be +ve or -ve)
+        /** Filter in positions in counts (can be +ve or -ve) */
         std::vector<int> in_positions_;
+        /** Filter out positions in counts (can be +ve or -ve) */
         std::vector<int> out_positions_;
-        // Thresholds for histogram bins above which some action should be taken
+        /** Thresholds for histogram bins above which some action should be taken */
         std::map<std::string, uint64_t> pixel_count_thresholds_;
 
         bool _handle_request(const json& request, json& response);
