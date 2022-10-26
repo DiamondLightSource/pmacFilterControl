8ccf7cd422d35e29a096e8cd267cc1dbf306e422
Ollie Copping <oliver.copping@diamond.ac.uk>
2022-10-26 16:36:48 +0100

    Added a ZMQ adapter script to connect to the zmq stream, and to send messages and receive responses

---
+++ zmqadapter.py
@@ -0,0 +1,96 @@
+import asyncio
+import logging
+from dataclasses import dataclass, field
+from typing import Callable, Iterable
+
+import aiozmq
+import zmq
+
+# LOGGER = logging.getLogger("ZmqAdapter")
+
+
+@dataclass
+class ZeroMQAdapter:
+    """An adapter for a ZeroMQ data stream."""
+
+    zmq_host: str = "127.0.0.1"
+    zmq_port: int = 5555
+    running: bool = False
+    _send_message_queue: asyncio.Queue = field(default_factory=asyncio.Queue)
+    _recv_message_queue: asyncio.Queue = field(default_factory=asyncio.Queue)
+
+    async def start_stream(self) -> None:
+        """Start the ZeroMQ stream."""
+        # LOGGER.debug("Starting stream...")
+        print("starting stream...")
+
+        self._socket = await aiozmq.create_zmq_stream(
+            zmq.REQ, connect=f"tcp://{self.zmq_host}:{self.zmq_port}"
+        )
+        # LOGGER.debug(f"Stream started. {self._socket}")
+        print(f"Stream started. {self._socket}")
+
+    async def close_stream(self) -> None:
+        """Close the ZeroMQ stream."""
+        self._socket.close()
+
+        self.running = False
+
+    def send_message(self, message: bytes) -> None:
+        """Send a message down the ZeroMQ stream.
+
+        Sets up an asyncio task to put the message on the message queue, before
+        being processed.
+
+        Args:
+            message (str): The message to send down the ZeroMQ stream.
+        """
+        self._send_message_queue.put_nowait(message)
+
+    async def _read_response(self) -> bytes:
+        resp = await self._socket.read()
+        return resp
+
+    async def get_response(self) -> bytes:
+        return await self._recv_message_queue.get()
+
+    async def run_forever(self) -> None:
+        """Runs the ZeroMQ adapter continuously."""
+        await self.start_stream()
+        self.running = True
+
+        await asyncio.gather(
+            *[
+                self._process_message_queue(),
+                self._process_response_queue(),
+            ]
+        )
+
+    def check_if_running(self):
+        """Returns the running state of the adapter."""
+        return self.running
+
+    async def _process_message_queue(self) -> None:
+        print("Processing message queue...")
+        running = True
+        while running:
+            message = await self._send_message_queue.get()
+            await self._process_message(message)
+            running = self.check_if_running()
+
+    async def _process_message(self, message: Iterable[bytes]) -> None:
+        if message is not None:
+            print(f"writing message: {message}")
+            self._socket.write(message)
+        else:
+            # LOGGER.debug("No message")
+            print("No message")
+
+    async def _process_response_queue(self) -> None:
+        print("Processing response queue...")
+        running = True
+        while running:
+            resp = await self._read_response()
+            print(f"got {resp}")
+            self._recv_message_queue.put_nowait(resp)
+            running = self.check_if_running()
