d1b67119a9f8f60f48d37f18d0e32f2f8d6c6020
Ollie Copping <oliver.copping@diamond.ac.uk>
2023-01-03 14:56:49 +0000

    Removed some logging as these were making it too verbose and aren't needed anymore

--- src/pmacfiltercontrol/pmacFilterControlWrapper.py
+++ src/pmacfiltercontrol/pmacFilterControlWrapper.py
@@ -541,7 +541,6 @@ class Wrapper:
 
     @_if_connected
     async def _clear_error(self, _) -> None:
-        print(self.clear_error.get())
 
         if _ == 1:
             clear_error = json.dumps({"command": "clear_error"})
